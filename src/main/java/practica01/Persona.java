5def2d307edea07280ff30fb419d506005b3f839
Andres Padilla <jandrespadilla@gmail.com>
2024-10-22 19:58:57 -0300

    Subo clase persona y main para mostrar datos en consola

--- src/main/java/practica01/Persona.java
+++ src/main/java/practica01/Persona.java
@@ -1,5 +1,38 @@
 package practica01;
 
 public class Persona {
+	private String nombre;
+	private String apellido;
+	private int dni;
+	private int edad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getDni() {
+		return dni;
+	}
 
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", edad=" + edad + "]";
+	}
 }
--- src/main/java/practica01/main.java
+++ src/main/java/practica01/main.java
@@ -1,10 +1,68 @@
 package practica01;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//genero las 5 personas
+		Persona persona01 = new Persona();
+		Persona persona02 = new Persona();
+		Persona persona03 = new Persona();
+		Persona persona04 = new Persona();
+		Persona persona05 = new Persona();
+		//Le asigno los valores nombre y apellido a cada persona con su respectivo setter
+		persona01.setNombre("Andres");
+		persona01.setApellido("Padilla");
+		persona02.setNombre("Adrian");
+		persona02.setApellido("Muzza");
+		persona03.setNombre("Federico");
+		persona03.setApellido("Ferrer");
+		persona04.setNombre("Hugo");
+		persona04.setApellido("Sanchez");
+		persona05.setNombre("Sebastian");
+		persona05.setApellido("Gaitan");
+		
+	    // creor una lista para almacenar las personas
+        ArrayList<Persona> listaPersonas = new ArrayList<>();
 
+        // agrego las personas a la lista
+        listaPersonas.add(persona01);
+        listaPersonas.add(persona02);
+        listaPersonas.add(persona03);
+        listaPersonas.add(persona04);
+        listaPersonas.add(persona05);
+
+        // imprimo los nombres y apellidos de las personas en la lista
+        System.out.println("Lista como se cargo");
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getNombre() + " " + persona.getApellido());
+        }
+     
+        
+        // Ordenar la lista por el nombre de la persona
+        Collections.sort(listaPersonas, new Comparator<Persona>() {
+            @Override
+            public int compare(Persona p1, Persona p2) {
+                return p1.getNombre().compareTo(p2.getNombre());
+            }
+        });        
+        System.out.println("\nLista ordenada por nombre:");
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getNombre() + " " + persona.getApellido());
+        }
+        Collections.sort(listaPersonas, new Comparator<Persona>() {
+            @Override
+            public int compare(Persona p1, Persona p2) {
+                return p2.getApellido().compareTo(p1.getApellido()); // Orden inverso
+            }
+        });
+        System.out.println("\nLista ordenada por Apellido:");
+        for (Persona persona : listaPersonas) {
+            System.out.println(persona.getNombre() + " " + persona.getApellido());
+        }
 	}
 
 }
